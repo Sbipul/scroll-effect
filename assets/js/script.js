4ccc284001c972236cfca5e8259f9fbf8e962544
bipul <kumarroy.kb.kb@gmail.com>
2022-01-21 15:36:09 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll',()=>{
+  let boy = document.getElementById('fixDiv2')
+  let value = window.scrollY;
+  console.log(value / 100)
+  console.log(window.innerWidth)
+  if (window.innerWidth <= 425) {
+    boy.style.left = ((value/100) * .74) + '%'
+  } else {
+    boy.style.left = ((value/100) * 2.96789) + '%'
+  }
+})
+
